f6df7f62e46699264dc37e075730e7651d9e8984
Brian Michael <brian.michael@ncr.com>
2019-02-07 15:47:31 -0500

    Adding swagger

---
+++ groundschool-app/src/main/java/com/starfireaviation/groundschool/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+/*
+ *-----------------------------------------------------------------------------
+ * Copyright 2019 Starfire Aviation, LLC
+ *-----------------------------------------------------------------------------
+ */
+package com.starfireaviation.groundschool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * SwaggerConfig
+ *
+ * @author brianmichael
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    /**
+     * Docket
+     *
+     * @return Docket
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
