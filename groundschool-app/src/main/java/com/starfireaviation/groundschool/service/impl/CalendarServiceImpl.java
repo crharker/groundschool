1f94ffa534d1e91c53c24f54dac5d4dbaf0da24d
Brian Michael <brian.michael@ncr.com>
2019-02-07 12:55:57 -0500

    Cleaning up dependencies

--- groundschool-app/src/main/java/com/starfireaviation/groundschool/service/impl/CalendarServiceImpl.java
+++ groundschool-app/src/main/java/com/starfireaviation/groundschool/service/impl/CalendarServiceImpl.java
@@ -6,34 +6,23 @@
 package com.starfireaviation.groundschool.service.impl;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Event.Source;
 import com.google.api.services.calendar.model.EventDateTime;
@@ -74,20 +63,10 @@ public class CalendarServiceImpl implements CalendarService {
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
     /**
-     * TOKENS_DIRECTORY_PATH
-     */
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-    /**
      * Global instance of the scopes required by this quickstart. If modifying these scopes, delete your previously
      * saved tokens/ folder.
      */
-    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
-
-    /**
-     * CREDENTIALS_FILE_PATH
-     */
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    //private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
 
     /**
      * EventService
@@ -204,7 +183,7 @@ public class CalendarServiceImpl implements CalendarService {
      * {@inheritDoc} Required implementation.
      */
     @Override
-    public boolean delete(Long eventId) throws ResourceNotFoundException {
+    public boolean delete(Long eventId) {
         // TODO Implement this!
         return false;
     }
@@ -231,20 +210,20 @@ public class CalendarServiceImpl implements CalendarService {
      */
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
-        InputStream in = CalendarServiceImpl.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        //InputStream in = CalendarServiceImpl.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        //GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT,
-                JSON_FACTORY,
-                clientSecrets,
-                SCOPES)
-                        .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                        .setAccessType("offline")
-                        .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        //new GoogleAuthorizationCodeFlow.Builder(
+        //        HTTP_TRANSPORT,
+        //        JSON_FACTORY,
+        //        clientSecrets,
+        //        SCOPES)
+        //                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+        //                .setAccessType("offline")
+        //                .build();
+        //LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        //return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        return null;
     }
 
     /**
