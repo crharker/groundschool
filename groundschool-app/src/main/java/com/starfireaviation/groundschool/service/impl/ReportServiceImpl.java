75774cf9737c7813adc740c654dfa596a4219adc
Brian Michael <brian.michael@ncr.com>
2019-02-13 08:45:40 -0500

    Adding Quiz Completion and Quiz Results charts

--- groundschool-app/src/main/java/com/starfireaviation/groundschool/service/impl/ReportServiceImpl.java
+++ groundschool-app/src/main/java/com/starfireaviation/groundschool/service/impl/ReportServiceImpl.java
@@ -5,18 +5,36 @@
  */
 package com.starfireaviation.groundschool.service.impl;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.apache.commons.collections4.CollectionUtils;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.starfireaviation.groundschool.model.sql.UserQuestionEntity;
-import com.starfireaviation.groundschool.repository.UserQuestionRepository;
+import com.starfireaviation.groundschool.exception.ResourceNotFoundException;
+import com.starfireaviation.groundschool.model.Question;
+import com.starfireaviation.groundschool.model.Quiz;
+import com.starfireaviation.groundschool.model.Statistic;
+import com.starfireaviation.groundschool.model.StatisticType;
+import com.starfireaviation.groundschool.service.QuizService;
 import com.starfireaviation.groundschool.service.ReportService;
+import com.starfireaviation.groundschool.service.StatisticService;
 
 /**
  * ReportServiceImpl
@@ -27,84 +45,193 @@ import com.starfireaviation.groundschool.service.ReportService;
 public class ReportServiceImpl implements ReportService {
 
     /**
-     * UserQuestionRepository
+     * Logger
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReportServiceImpl.class);
+
+    /**
+     * QUESTION_ANSWERED_DESC_PATTERN
+     */
+    private static final Pattern QUESTION_ANSWERED_DESC_PATTERN = Pattern.compile(
+            "Duration [(.+)]; Answer Given [(.+)]; Answered Correctly [(.+)]");
+
+    /**
+     * QuizService
      */
     @Autowired
-    private UserQuestionRepository userQuestionRepository;
+    private QuizService quizService;
 
     /**
-     * Initializes an instance of <code>ReportServiceImpl</code> with the default data.
+     * StatisticService
      */
-    public ReportServiceImpl() {
-        // Default constructor
-    }
+    @Autowired
+    private StatisticService statisticService;
 
     /**
-     * Initializes an instance of <code>ReportServiceImpl</code> with the default data.
-     *
-     * @param userQuestionRepository UserQuestionRepository
+     * NOT_ANSWERED
      */
-    public ReportServiceImpl(UserQuestionRepository userQuestionRepository) {
-        this.userQuestionRepository = userQuestionRepository;
-    }
+    private static final String NOT_ANSWERED = "Not Answered";
+
+    /**
+     * ANSWERED_CORRECTLY
+     */
+    private static final String ANSWERED_CORRECTLY = "Answered Correctly";
+
+    /**
+     * ANSWERED_INCORRECTLY
+     */
+    private static final String ANSWERED_INCORRECTLY = "Answered Incorrectly";
+
+    /**
+     * QUESTION_NUM
+     */
+    private static final String QUESTION_NUM = "Question #";
+
+    /**
+     * GREEN
+     */
+    private static final Color GREEN = new Color(0, 255, 0);
+
+    /**
+     * BLUE
+     */
+    private static final Color BLUE = new Color(0, 0, 255);
+
+    /**
+     * RED
+     */
+    private static final Color RED = new Color(255, 0, 0);
 
     /**
      * {@inheritDoc} Required implementation.
      */
     @Override
-    public List<Map<String, String>> getAllAnswersForUser(Long userId) {
-        final List<Map<String, String>> response = new ArrayList<>();
-        final List<UserQuestionEntity> userQuestionEntities = userQuestionRepository.findByUserId(userId);
-        buildResponse(response, userQuestionEntities);
-        return response;
-    }
-
-    /**
-     * {@inheritDoc} Required implementation.
-     */
-    @Override
-    public List<Map<String, String>> getAllAnswersForEvent(Long eventId) {
-        final List<Map<String, String>> response = new ArrayList<>();
-        final List<UserQuestionEntity> userQuestionEntities = userQuestionRepository.findByEventId(eventId);
-        buildResponse(response, userQuestionEntities);
-        return response;
-    }
-
-    /**
-     * {@inheritDoc} Required implementation.
-     */
-    @Override
-    public List<Map<String, String>> getAllAnswersForQuiz(Long quizId) {
-        final List<Map<String, String>> response = new ArrayList<>();
-        final List<UserQuestionEntity> userQuestionEntities = userQuestionRepository.findByQuizId(quizId);
-        buildResponse(response, userQuestionEntities);
-        return response;
-    }
-
-    /**
-     * Builds response list
-     *
-     * @param response list of map data
-     * @param userQuestionEntities database data
-     */
-    private static void buildResponse(
-            final List<Map<String, String>> response,
-            final List<UserQuestionEntity> userQuestionEntities) {
-        if (!CollectionUtils.isEmpty(userQuestionEntities)) {
-            for (final UserQuestionEntity userQuestionEntity : userQuestionEntities) {
-                Map<String, String> map = new HashMap<>();
-                map.put(UserQuestionEntity.ANSWER_GIVEN, userQuestionEntity.getAnswerGiven());
-                map.put(
-                        UserQuestionEntity.ANSWERED_CORRECTLY,
-                        Boolean.toString(userQuestionEntity.isAnsweredCorrectly()));
-                map.put(UserQuestionEntity.EVENT_ID, Long.toString(userQuestionEntity.getEventId()));
-                map.put(UserQuestionEntity.QUESTION_ID, Long.toString(userQuestionEntity.getQuestionId()));
-                map.put(UserQuestionEntity.QUIZ_ID, Long.toString(userQuestionEntity.getQuizId()));
-                //map.put(UserQuestionEntity.TIME, userQuestionEntity.getTime());
-                map.put(UserQuestionEntity.USER_ID, Long.toString(userQuestionEntity.getUserId()));
-                response.add(map);
+    public JFreeChart getQuizCompletionChart(Long quizId) throws ResourceNotFoundException {
+        final Quiz quiz = quizService.get(quizId);
+        final List<Question> questions = getQuizQuestions(quiz);
+        final List<Long> quizParticipants = new ArrayList<>();
+        int answeredCorrectly = 0;
+        int answeredIncorrectly = 0;
+        final List<Statistic> statistics = statisticService.findByQuizId(quizId, StatisticType.QUESTION_ANSWERED);
+        for (final Statistic statistic : statistics) {
+            boolean correct = false;
+            final Matcher matcher = QUESTION_ANSWERED_DESC_PATTERN.matcher(statistic.getDescription());
+            if (matcher.find()) {
+                LOGGER.info("getQuizCompletionChart() matching on " + matcher.group(3));
+                correct = Boolean.valueOf(matcher.group(3));
+            }
+            if (correct) {
+                answeredCorrectly += 1;
+            } else {
+                answeredIncorrectly += 1;
+            }
+            if (!quizParticipants.contains(statistic.getUserId())) {
+                quizParticipants.add(statistic.getUserId());
             }
         }
+        final int notAnswered = (questions.size() * quizParticipants.size())
+                - (answeredCorrectly + answeredIncorrectly);
+        final DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue(NOT_ANSWERED, notAnswered);
+        dataset.setValue(ANSWERED_CORRECTLY, answeredCorrectly);
+        dataset.setValue(ANSWERED_INCORRECTLY, answeredIncorrectly);
+        final JFreeChart chart = ChartFactory.createPieChart(
+                String.format("Quiz \"%s\" Completion", quiz.getTitle()),
+                dataset);
+        final PiePlot plot = (PiePlot) chart.getPlot();
+        plot.setSectionPaint(NOT_ANSWERED, BLUE);
+        plot.setSectionPaint(ANSWERED_CORRECTLY, GREEN);
+        plot.setSectionPaint(ANSWERED_INCORRECTLY, RED);
+        return chart;
+    }
+
+    /**
+     * {@inheritDoc} Required implementation.
+     */
+    @Override
+    public JFreeChart getQuizResultsChart(Long quizId) throws ResourceNotFoundException {
+        final Quiz quiz = quizService.get(quizId);
+        final List<Question> questions = getQuizQuestions(quiz);
+        final Map<Long, Integer> questionIdMap = buildQuestionIdMap(questions);
+        final int questionData[][] = new int[questions.size()][3];
+        final List<Long> quizParticipants = new ArrayList<>();
+        final List<Statistic> statistics = statisticService.findByQuizId(quizId, StatisticType.QUESTION_ANSWERED);
+        for (Statistic statistic : statistics) {
+            boolean correct = false;
+            final Matcher matcher = QUESTION_ANSWERED_DESC_PATTERN.matcher(statistic.getDescription());
+            if (matcher.find()) {
+                LOGGER.info("getQuizCompletionChart() matching on " + matcher.group(3));
+                correct = Boolean.valueOf(matcher.group(3));
+            }
+            final int questionIndex = questionIdMap.get(statistic.getQuestionId());
+            if (correct) {
+                questionData[questionIndex][1] += 1;
+            } else {
+                questionData[questionIndex][2] += 1;
+            }
+            if (!quizParticipants.contains(statistic.getUserId())) {
+                quizParticipants.add(statistic.getUserId());
+            }
+        }
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (int i = 0; i < questionData.length; i++) {
+            final int answered = questionData[i][1] + questionData[i][2];
+            if (answered < quizParticipants.size()) {
+                questionData[i][0] = quizParticipants.size() - answered;
+            } else {
+                questionData[i][0] = 0;
+            }
+            dataset.addValue(questionData[i][0], NOT_ANSWERED, QUESTION_NUM + (i + 1));
+            dataset.addValue(questionData[i][1], ANSWERED_CORRECTLY, QUESTION_NUM + (i + 1));
+            dataset.addValue(questionData[i][2], ANSWERED_INCORRECTLY, QUESTION_NUM + (i + 1));
+        }
+        final JFreeChart chart = ChartFactory.createBarChart(
+                String.format("Quiz \"%s\" Results", quiz.getTitle()),
+                "Question",
+                "# of Participants Answering",
+                dataset);
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+        BarRenderer renderer = (BarRenderer) plot.getRenderer();
+        renderer.setSeriesPaint(0, BLUE);
+        renderer.setSeriesPaint(1, GREEN);
+        renderer.setSeriesPaint(2, RED);
+        CategoryAxis domainAxis = new CategoryAxis();
+        domainAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_45);
+        plot.setDomainAxis(domainAxis);
+        return chart;
+    }
+
+    /**
+     * Verifies input data as well as retrieves quiz question list
+     *
+     * @param quiz Quiz
+     * @return list of questions
+     * @throws ResourceNotFoundException when quiz or quiz questions are not found
+     */
+    private static List<Question> getQuizQuestions(final Quiz quiz) throws ResourceNotFoundException {
+        if (quiz == null) {
+            throw new ResourceNotFoundException("Quiz not found");
+        }
+        final List<Question> questions = quiz.getQuestions();
+        if (questions == null) {
+            throw new ResourceNotFoundException("Quiz questions not found");
+        }
+        return questions;
+    }
+
+    /**
+     * Builds a question id map
+     *
+     * @param questions list of questions
+     * @return Map
+     */
+    private static Map<Long, Integer> buildQuestionIdMap(final List<Question> questions) {
+        final Map<Long, Integer> questionIdMap = new HashMap<>();
+        int count = 0;
+        for (Question question : questions) {
+            questionIdMap.put(question.getId(), count++);
+        }
+        return questionIdMap;
     }
 
 }
