7deeb0ad9b2ac3a9e8880b2500d6704c0cde3147
Brian Michael <brian.michael@ncr.com>
2019-02-24 12:48:35 -0500

    Adding quiz and event component implementation

--- groundschool-ui/src/app/event/event.component.ts
+++ groundschool-ui/src/app/event/event.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from '@app/_models';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AlertService, EventService } from '@app/_services';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-event',
@@ -6,10 +11,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
+  eventForm: FormGroup;
+  events: Event[] = [];
+  event: Event;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private eventService: EventService,
+    private alertService: AlertService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+    this.eventForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      startTime: ['', Validators.required],
+      lessonPlanId: ['', Validators.required],
+      address: ['', Validators.required]
+    });
   }
 
+    // convenience getter for easy access to form fields
+    get f() { return this.eventForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.eventForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        if (this.eventForm.value.id) {
+          this.eventService
+              .create(this.eventForm.value)
+              .pipe(first())
+              .subscribe(
+                  data => {
+                      this.router.navigate([this.returnUrl]);
+                  },
+                  error => {
+                      this.alertService.error(error);
+                      this.loading = false;
+                  }
+          );
+        } else {
+          this.eventService
+              .update(this.eventForm.value)
+              .pipe(first())
+              .subscribe(
+                  data => {
+                      this.router.navigate([this.returnUrl]);
+                  },
+                  error => {
+                      this.alertService.error(error);
+                      this.loading = false;
+                  }
+          );
+        }
+    }
 }
--- groundschool-ui/src/app/quiz/quiz.component.ts
+++ groundschool-ui/src/app/quiz/quiz.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Quiz } from '@app/_models';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AlertService, QuizService } from '@app/_services';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-quiz',
@@ -6,10 +11,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
+  quizForm: FormGroup;
+  quizzes: Quiz[] = [];
+  quiz: Quiz;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private quizService: QuizService,
+    private alertService: AlertService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+    this.quizForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      lessonPlanId: ['', Validators.required],
+      questions: ['', Validators.required],
+      quizType: ['', Validators.required]
+    });
   }
 
+    // convenience getter for easy access to form fields
+    get f() { return this.quizForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.quizForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        if (this.quizForm.value.id) {
+          this.quizService
+              .create(this.quizForm.value)
+              .pipe(first())
+              .subscribe(
+                  data => {
+                      this.router.navigate([this.returnUrl]);
+                  },
+                  error => {
+                      this.alertService.error(error);
+                      this.loading = false;
+                  }
+          );
+        } else {
+          this.quizService
+              .update(this.quizForm.value)
+              .pipe(first())
+              .subscribe(
+                  data => {
+                      this.router.navigate([this.returnUrl]);
+                  },
+                  error => {
+                      this.alertService.error(error);
+                      this.loading = false;
+                  }
+          );
+        }
+    }
 }
